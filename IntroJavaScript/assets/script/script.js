f7cb943d34b0d8fd73caa5a005ae33ba75b91e81
Koose <KooseTheBlogger@gmail.com>
2022-02-28 11:15:13 +0000

    onclick

--- IntroJavaScript/assets/script/script.js
+++ IntroJavaScript/assets/script/script.js
@@ -1,1 +1,17 @@
 alert("Welcome Open Labs")
+
+var myFirstName = 'Koose';
+var myAge = 21
+alert("welcome " + myFirstName + " my age is" + myAge)
+console.log(myFirstName)
+
+function sayHello(){
+    alert("Koose");
+
+}
+function storeData(){
+    document.write("info Stored")
+
+}
+document.getElementById("myBtn").onclick = sayHello;
+document.getElementById("Store").onclick = storeData;
