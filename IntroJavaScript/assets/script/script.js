faeaea81409cd5802616733d834947105a7b8ffa
Koose <KooseTheBlogger@gmail.com>
2022-03-02 13:39:21 +0000

    code working

--- IntroJavaScript/assets/script/script.js
+++ IntroJavaScript/assets/script/script.js
@@ -1,14 +1,33 @@
-var calculate = {
-    A1 : null,
-    A2 :null,
-    A3:null,
+var html ={
+    test1 :null,
+    test2:null,
+    total: null
 }
 
 
-function total()
-{
-        calculate.A1 = document.getElementById("A1").value;
-        calculate.A2 = document.getElementById("A2").value;
-        calculate.A3 = calculate.A1 + calculate.A2;
-        document.getElementById("result").innerHTML = calculate.A3
+function htmlfun(){
+    html.test1 = parseInt( document.getElementById("ht_1").value);
+    html.test2 = parseInt( document.getElementById("ht_2").value);
+    
+    html.total = html.test1 + html.test2;
+    document.getElementById("ht_total").innerHTML = html.total;
+
+}
+
+function Cssfun(){
+    html.test1 = parseInt( document.getElementById("ht_3").value);
+    html.test2 = parseInt( document.getElementById("ht_4").value);
+    
+    html.total = html.test1 + html.test2;
+    document.getElementById("css_total").innerHTML = html.total;
+
+}
+
+function JavaScriptfun(){
+    html.test1 = parseInt( document.getElementById("ht_5").value);
+    html.test2 = parseInt( document.getElementById("ht_6").value);
+    
+    html.total = html.test1 + html.test2;
+    document.getElementById("javascript_total").innerHTML = html.total;
+
 }
