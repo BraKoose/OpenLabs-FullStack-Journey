6dd32aba507c127e39eec46919b3c77334ae62e0
Koose <KooseTheBlogger@gmail.com>
2022-04-27 12:25:46 +0000

    More code

--- callbacks.js
+++ callbacks.js
@@ -6,3 +6,15 @@ var start = function(callback){
     callback()
 }
 start(welcomeMsg)
+
+var look = function(dir){
+    if(dir ==='west')
+    {
+        console.log
+        ("There is a small mailbox here.");
+    if (dir === 'east');
+    {
+        console.log("You are standing next a white house.");
+    }    
+    }
+}
