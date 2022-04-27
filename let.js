533ca18aed9f739a781a147dd5a81068186b4421
Koose <KooseTheBlogger@gmail.com>
2022-04-27 03:59:50 +0000

    NodeJs

---
+++ let.js
@@ -0,0 +1,8 @@
+
+    let count = 100
+    for (count =1; count <=10; count++){
+        //inside for loops brackets, count value start from 1
+console.log("count value inside loop is" + count);        
+    }
+    //Outside loop brackets, count value is 100
+    console.log("count value after loop is" + count)
