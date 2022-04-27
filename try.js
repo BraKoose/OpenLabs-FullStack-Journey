a5abd27f998ead63e6859c2fdbb90e099f8a86b9
Koose <KooseTheBlogger@gmail.com>
2022-04-27 16:46:49 +0000

    callback working

--- try.js
+++ try.js
@@ -1,11 +1,23 @@
 var http = require("http");
+var fileS = require("fs");
+
+
 
 http.createServer(function (req, res) {
+    fileS.readFile('data.html', (err, data) => {
+        console.log(data);
 
-    res.writeHead(200, {'Content-Type': 'html'});
-   
-res.end('This is the example of node.js web-based application \n');
+        res.writeHead(200, { 'Content-Type': 'html' });
+
+        res.write(data)
+    })
+
+
+
+
+    return res.end
+
 
 
 }).listen(9090,
-    ()=>console.log('Server running at http://127.0.0.1:9090/'));
+    () => console.log('Server running ...... 0n port 9090'));
