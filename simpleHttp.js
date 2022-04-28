9a2357d0734fe209f872ca3bc9968de377b787a3
Koose <KooseTheBlogger@gmail.com>
2022-04-28 06:02:55 +0000

    listening on port 9090, / routing too

--- simpleHttp.js
+++ simpleHttp.js
@@ -1,9 +1,20 @@
 var http = require('http');
 
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'Text/plain' });
 
-    res.end("Hello");
+var server = http.createServer(function (req, res) {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end("Hello");
+    } else if (req.url === '/blog') {
+        if (req.method === 'GET') {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end("Blog Money");
+        } else if (req.method === 'POST') {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end("Blog Wirter");
+        }
+    }
+
 });
 
 server.listen(9090,
