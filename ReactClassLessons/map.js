80fc13d6e501eaf563a898995176f21098723a26
Koose <KooseTheBlogger@gmail.com>
2022-05-04 12:08:00 +0000

    empty array

--- ReactClassLessons/map.js
+++ ReactClassLessons/map.js
@@ -2,12 +2,12 @@ const removeSubdocs = require("mongoose/lib/plugins/removeSubdocs");
 
 var arr = new Array(1, 2, 3, 4, 5, 6, 8, 9, 10)
 
-let koose = new Array(['Auntie Mumuni', 'Reggie RockStone', 'Koko King']);
+let koose = new Array();
 
 function foodSellers(f) {
     return f + 1;
 
 }
 
-result = koose.map(foodSellers)
+result = arr.map(foodSellers)
 console.log(result)
