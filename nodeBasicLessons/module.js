08facdd8e962c0f13dff15c3415a27b316c796d6
Koose <KooseTheBlogger@gmail.com>
2022-05-04 17:42:13 +0000

    modules and the fileSysten

---
+++ nodeBasicLessons/module.js
@@ -0,0 +1,7 @@
+const abc = require('./people')
+
+console.log(abc.people, abc.ages);
+
+const os = require('os');
+
+console.log(os.platform, os.homedir());
---
+++ nodeBasicLessons/people.js
@@ -0,0 +1,8 @@
+const people = ['Kooko', 'Koose', 'ELevy', 'Absa'];
+const ages = [20, 40, 60, 80]
+
+console.log(people)
+
+module.exports = {
+    people, ages
+}
