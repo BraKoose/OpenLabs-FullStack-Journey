021560f207479ee30a98bed361405139cd2573c6
Koose <KooseTheBlogger@gmail.com>
2022-05-06 10:28:13 +0000

    deleted files

--- nodeBasicLessons/module.js
+++
@@ -1,7 +0,0 @@
-const abc = require('./people')
-
-console.log(abc.people, abc.ages);
-
-const os = require('os');
-
-console.log(os.platform, os.homedir());
--- nodeBasicLessons/people.js
+++
@@ -1,8 +0,0 @@
-const people = ['Kooko', 'Koose', 'ELevy', 'Absa'];
-const ages = [20, 40, 60, 80]
-
-console.log(people)
-
-module.exports = {
-    people, ages
-}
