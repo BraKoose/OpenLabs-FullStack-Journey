17ba78506ca0a3ed8f2dca2a76296681b3cd74c4
Koose <KooseTheBlogger@gmail.com>
2022-05-06 13:50:46 +0000

    fs to readFile of the Html data

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const http = require('http');
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    //set Header content type 
+
+    res.setHeader('Content-Type', 'text/html');
+
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.write(data);
+            res.end();
+        }
+    })
+
 });
 
 server.listen(8990, 'localhost', () => {
