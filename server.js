1b62ecd799acda5d27dee43ac9e434e7da5e9e2b
Koose <KooseTheBlogger@gmail.com>
2022-05-06 14:47:31 +0000

    node routing

--- server.js
+++ server.js
@@ -8,7 +8,20 @@ const server = http.createServer((req, res) => {
 
     res.setHeader('Content-Type', 'text/html');
 
-    fs.readFile('./views/index.html', (err, data) => {
+    let path = './views/';
+    switch (req.url) {
+        case '/':
+            path += 'index.html'
+            break;
+        case '/about':
+            path += 'about.html'
+            break;
+        default:
+            path += '404.html';
+            break
+    }
+
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.log(err)
         }
