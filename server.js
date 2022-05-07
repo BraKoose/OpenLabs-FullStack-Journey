5e12495a68566b44ced7f3f37e100aa155893ab7
Koose <KooseTheBlogger@gmail.com>
2022-05-07 16:56:40 +0000

    lodash example to _.once run function once

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const http = require('http');
 const fs = require('fs')
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+
+    //lodash
+
+    const num = _.random(0, 23);
+    console.log(num)
+
+    const greet = _.once(() => {
+        console.log('hello Koose')
+    });
+
+    greet()
 
     //set Header content type 
 
