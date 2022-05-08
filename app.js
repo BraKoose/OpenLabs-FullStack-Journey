fc472981b122a6545bf9f26a8d7e792d7394b34e
Koose <KooseTheBlogger@gmail.com>
2022-05-08 07:37:52 +0000

    blog phase1

--- app.js
+++ app.js
@@ -4,19 +4,23 @@ const express = require('express')
 
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs');
+
+
 //listen for req
 
 app.listen(7676);
 
 app.get('/', (req, res) => {
     //  res.send('<p>Home Page</p>')
-    res.sendFile('./views/index.html', { root: __dirname })
+    res.render('index')
 
 });
 
 app.get('/about', (req, res) => {
     //  res.send('<p>Home Page</p>')
-    res.sendFile('./views/about.html', { root: __dirname })
+    res.render('about')
 
 });
 
@@ -28,6 +32,6 @@ app.get('/about-us', (req, res) => {
 
 //404 Pages ablways at the bottom of the routing 
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname })
+    res.status(404).render('404')
 
 });
